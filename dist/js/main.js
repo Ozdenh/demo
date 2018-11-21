483689456d9819e31b8b42900ba490f469f3ea29
Ozdenh <halavurt@mail.ru>
2018-11-21 06:30:13 +0300

    Add files via upload

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,7 @@
 const countdown = document.querySelector('.countdown');
 
 // Set Launch Date (ms)
-const launchDate = new Date('Nov 21, 2018 06:00:00').getTime();
+const launchDate = new Date('Nov 21, 2018 18:00:00').getTime();
 
 // Update every second
 const intvl = setInterval(() => {
